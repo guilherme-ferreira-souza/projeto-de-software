5e142ab986076bf49b1c46ae296b2e838b413190
guilherme-ferreira-souza <80338511+guilherme-ferreira-souza@users.noreply.github.com>
2022-07-06 19:35:26 -0300

    projeto

--- Python/yoursell01/base/urls.py
+++ Python/yoursell01/base/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from operator import concat
-from .views import indexView
+from .views import indexView, bundesligaView, premierlView, serieAView, menuView
 urlpatterns = [
     path('', indexView.as_view()),
+    path('', bundesligaView.as_view()),
+    path('', premierlView.as_view()),
+    path('', serieAView.as_view()),
+    path('', menuView.as_view()),
 ]
--- Python/yoursell01/base/views.py
+++ Python/yoursell01/base/views.py
@@ -4,9 +4,11 @@ from django.views.generic import TemplateView
 
 class indexView(TemplateView):
     template_name = "index.html"
-class indexView(TemplateView):
-    template_name = "index.html"
-class indexView(TemplateView):
-    template_name = "index.html"
-class indexView(TemplateView):
-    template_name = "index.html"
+class bundesligaView(TemplateView):
+    template_name = "bundesliga.html"
+class menuView(TemplateView):
+    template_name = "menu.html"
+class premierlView(TemplateView):
+    template_name = "premierl.html"
+class serieAView(TemplateView):
+    template_name = "premierl.html"
