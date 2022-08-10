dedc3346d8097da3e2aae9193a40ecf64b4a45a3
guilherme-ferreira-souza <80338511+guilherme-ferreira-souza@users.noreply.github.com>
2022-08-10 15:01:42 -0300

    commit

--- Python/yoursell01/base/urls.py
+++ Python/yoursell01/base/urls.py
@@ -3,7 +3,7 @@ from operator import concat
 from .views import indexView, bundesligaView, premierlView, serieAView, menuView
 
 urlpatterns = [
-    path('index/', indexView.as_view()),
+    path('', indexView.as_view()),
     path('bundesliga/', bundesligaView.as_view()),
     path('premierl/', premierlView.as_view()),
     path('serieA/', serieAView.as_view()),
